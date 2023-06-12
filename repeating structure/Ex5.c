47384a72bfd51b12e1d68ab3f56341816a6f0f74
ruan rocha <ruanrochaeuok@gmail.com>
2023-06-11 21:01:47 -0300

    13-[200~Today will let's make a program where the user enters several numbers, and then it display the :q

---
+++ repeating structure/Ex5.c
@@ -0,0 +1,2 @@
+/*Today will let's make a program where the user enters several numbers, and then it display the
+largets number and how many times it was entered. The user has to input the quantity of numbers.*/
