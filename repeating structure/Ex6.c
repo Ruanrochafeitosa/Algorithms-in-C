a53fe164c8127c371d0388a7e60e842b217bd256
ruan rocha <ruanrochaeuok@gmail.com>
2023-06-17 11:54:34 -0300

    19-Adding the sum of divisors

--- repeating structure/Ex6.c
+++ repeating structure/Ex6.c
@@ -1,4 +1,5 @@
-/*In this exercise, let's calculate the sum of divisors for the number N, excluding the number N itself.*/
+/*In this exercise, let's calculate the sum of divisors for the number N, excluding the number N itself, and
+adding the sum of divisors*/
 #include<stdio.h>
 int main(){
     int num;
@@ -8,7 +9,7 @@ int main(){
         printf("Enter a positive number:");
         scanf("%d", &num);
     }
-     int result[num], j=0;
+    int result[num], j=0;
     printf("The divirsors for the number %d is:", num);
     for(int i=1;i<num;i++){
         if(num%i==0){
@@ -16,13 +17,15 @@ int main(){
             j++;
         }
     }
+    int sum=0;
     for(int i=0;i<j;i++){
+        sum+=result[i];
         if(i==j-1){
         printf("and %d.", result[i]);
         }else{
             printf("%d, ", result[i]);
         }
     }
-
+    printf("\nThe sum os divisors:%d\n", sum);
     return 0;
 }
